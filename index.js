2f7c22ef3d057e793f0ed4c66a1de1bfdae060bc
Salif Saidou <104588234+SAIDOU11@users.noreply.github.com>
2023-06-03 06:30:58 +0200

    start project

---
+++ index.js
@@ -0,0 +1,9 @@
+// document.getElementById("count-el").innerText = 5;
+
+// let count = 0;
+// count = count + 10;
+// console.log(count);
+
+function increment() {
+  console.log("The button was clicked");
+}
